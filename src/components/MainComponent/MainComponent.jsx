cf2fe5abd7f250da51aae692a708f5a80766f375
unknown <phu21994@gmail.com>
2022-04-28 02:11:12 +0700

    Create Main component

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import "./MainComponent.css"
+import HeaderMainComponent from '@components/HeaderMainComponent'
+import ChatMainComponent from '@components/ChatMainComponent'
+import InputMainComponent from '@components/InputMainComponent'
 
 function MainComponent() {
   return (
-    <div className="main">Main</div>
+    <div className="main">
+      <HeaderMainComponent />
+      <ChatMainComponent />
+      <InputMainComponent />
+    </div>
   )
 }
 
