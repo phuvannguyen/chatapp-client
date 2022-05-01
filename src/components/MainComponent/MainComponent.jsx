47e179ca8ef0e248f09bf93b59b1f3b61ad18c7c
unknown <phu21994@gmail.com>
2022-05-01 22:34:50 +0700

    Delete Empty Component in Main component

--- src/components/MainComponent/MainComponent.jsx
+++ src/components/MainComponent/MainComponent.jsx
@@ -3,14 +3,14 @@ import "./MainComponent.css"
 import HeaderMainComponent from '@components/HeaderMainComponent'
 import ChatMainComponent from '@components/ChatMainComponent'
 import InputMainComponent from '@components/InputMainComponent'
-import EmptyComponent from '@components/EmptyComponent'
+
 
 function MainComponent() {
-  const trueOrFalse = Math.random() < 0.5;
+  
   return (
     <div className="main">
       <HeaderMainComponent />
-      {trueOrFalse ? <ChatMainComponent /> : <EmptyComponent />}
+      <ChatMainComponent />
       <InputMainComponent />
     </div>
   )
