901f03e00f04d115c9c03cef23302963229d1a42
Phu Nguyen <phu21994@gmail.com>
2022-06-16 18:07:26 +0700

    Handle logic for Login form

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,24 +1,53 @@
 import React from 'react'
 import "./LoginForm.css"
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 import GoogleIcon from '@mui/icons-material/Google'
 import GitHubIcon from '@mui/icons-material/GitHub'
 
-function LoginForm() {
-  return (
+function LoginForm({user, refs, validate, handleChange, handleSubmit}) {
+
+    const {username, password, error, success} = user;
+    const {form, checkBtn} =  refs;
+    const {required, vusername, validatePassword} = validate;
+  return ( 
+
     <div className="loginForm">
         <h1> Login</h1>
-        <form className='form'>
-            <input type="text" placeholder='Username' />
-            <input type="password" placeholder='Password' />
-            <a href='#'>Forget password?</a>
-            <label>
-                <input type="checkbox" /> 
-                Remember
-            </label>
-            <button type='submit'>Login</button>        
+        <Form ref={form}>
+            {!success && (
+                <div className="form">
+                    <Input  type="text" placeholder='Username' 
+                            name="username" 
+                            onChange={handleChange} 
+                            value={username} 
+                            validations={[required, vusername]}/>
+                    <Input  type="password" 
+                            placeholder='Password' 
+                            name="password" 
+                            onChange={handleChange} 
+                            value={password} 
+                            validations={[required, validatePassword]}/>
+                    <a href='#'>Forget password?</a>
+                    <label>
+                        <input type="checkbox" /> 
+                        Remember
+                    </label>
+                    <button type='submit' onClick={handleSubmit}>Login</button>
+
+                </div>
+            )}
+            {error && (
+              <div className={success ? "alert alert-success" : "alert alert-danger" }>
+                {error}
+              </div>
+          )}
+
+            <CheckButton style={{ display: "none" }} ref={refs.checkBtn} />          
 
 
-        </form>
+        </Form>
         <h3>
                 Don't have a account ? <a href='/signup'>Sign Up</a>
         </h3>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,97 @@
-import LoginForm from '@components/LoginForm'
-import React from 'react'
+import LoginForm  from '@components/LoginForm'
+import React, {useState, useRef} from 'react'
+import axios from "axios"
 
 function Login() {
+  const form = useRef();
+  const checkBtn = useRef();
+  const [user, setUser] = useState({
+    username: "",    
+    password: "",
+    error: "",
+    success: false    
+  });
+     
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name =  e.target.name;      
+    setUser({...user, [name] : value});  
+    
+
+  };
+
+  // handle emtpy value in username, password, email and confirm email
+  const required = (value) => {
+    if (!value) {
+      <div className="alert alert-danger">
+        <span>This field is required!</span>
+        <a class="close">&times;</a>
+      </div>
+    }
+  };
+
+  const vusername = (value) => {
+    if (value.length < 3 || value.length > 20) {
+      return (
+        <div className="alert alert-danger">
+          <span>The username must be between 3 and 20 characters.</span>
+          <a class="close">&times;</a>
+        </div>
+      );
+    }
+  };  
+
+  const validatePassword = (value) => {
+    if (value.length < 6 || value.length > 40) {
+      return (
+        <div className="alert alert-danger" >
+          The password must be between 6 and 40 characters.
+        </div>
+      );
+    };   
+     
+  }
+  
+  
+
+  const handleSubmit = (e) => {
+    const {username, password} = user;
+    e.preventDefault();
+    form.current.validateAll();
+    if (checkBtn.current.context._errors.length === 0) {
+      axios.post('http://localhost:8080/api/login', {
+        username, password
+      })
+      .then(function (response) {
+        setUser({
+          ...user,
+          error: "Login sucess",
+          success: true
+        });
+        console.log(response)
+      })
+      .catch(function (error) {
+        setUser({
+          ...user,
+          error: error.response.data,
+          success: false
+        });
+        console.log(error)
+      });
+  
+    }
+
+    }   
+    
+    
+
+
   return (
-    <LoginForm />
+    <LoginForm user={user} 
+               handleChange={handleChange} 
+               handleSubmit={handleSubmit} 
+               refs={{form, checkBtn}} 
+               validate={{required, vusername, validatePassword}}/>
   )
 }
 
