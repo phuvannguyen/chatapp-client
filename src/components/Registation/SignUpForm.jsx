5ba2e89d5eafb03d197d80c40d311fc5396076b4
Phu Nguyen <phu21994@gmail.com>
2022-06-16 18:05:18 +0700

    Handle logic for signup page

--- src/components/Registation/SignUpForm.jsx
+++ src/components/Registation/SignUpForm.jsx
@@ -1,18 +1,37 @@
 import React from 'react'
 import "./SignUpForm.css"
+import Form from "react-validation/build/form";
+import Input from "react-validation/build/input";
+import CheckButton from "react-validation/build/button";
 
-function SignUpForm() {
+
+
+function SignUpForm({user, handleChange, handleSubmit, refs, validate}) {
+  const {required, vusername, validateEmail, validatePassword, confirmPassword} = validate;
   return (
     <div className="signup-form">
-      <h1>Sign Up</h1>
-      <form className="form-signup">
-        <input type="text" className="username" placeholder='User name'/>
-        <input type="email" className='email' placeholder='Email'/>
-        <input type="password" placeholder='Password'/>
-        <input type="password" placeholder='Confirm password' />
-        <button type="submit">Sign Up</button>
-      </form>
-      <h3>Do you have a account? <a href="/">Login</a></h3>     
+      <h1>Sign Up</h1>    
+      
+          <Form  onSubmit={handleSubmit} ref={refs.form}>
+            {!user.success && (
+              <div className="form-signup">
+                <Input type="text" className="username" placeholder='User name' name="username" onChange={handleChange} value={user.username} validations={[required, vusername]}/>
+                <Input type="email" className='email' placeholder='Email' name="email" onChange={handleChange} value={user.email} validations={[required, validateEmail]}/>
+                <Input type="password" placeholder='Password' name="password" onChange={handleChange} value={user.password} validations={[required, validatePassword, ]}/>
+                <Input type="password" placeholder='Confirm password' name="confirm_password" onChange={handleChange} value={user.confirm_password} validations={[required, confirmPassword]} />
+                <button type="submit" >Sign Up</button>                
+              </div>
+            )}
+            {user.error && (
+              <div className={ user.success ? "alert alert-success" : "alert alert-danger" }>
+                {user.error}
+              </div>
+          )}
+            <CheckButton style={{ display: "none" }} ref={refs.checkBtn} />                
+            
+          </Form>
+          <h3>Do you have a account? <a href="/">Login</a></h3>     
+           
       
     </div>
   )
--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,118 @@
-import React from 'react'
+import React, {useState, useRef} from 'react'
+import axios from "axios"
 import SignUpForm from '@components/Registation'
 
 function SignUp() {
+  const form = useRef();
+  const checkBtn = useRef();
+
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirm_password: "",
+    error: "",
+    success: false
+  });
+     
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const name =  e.target.name;      
+    setUser({...user, [name] : value});  
+    
+
+  };
+  // handle emtpy value in username, password, email and confirm email
+  const required = (value) => {
+    if (!value) {
+      <div className="alert alert-danger">
+        <span>This field is required!</span>
+        <a class="close">&times;</a>
+      </div>
+    }
+  };
+
+  const vusername = (value) => {
+    if (value.length < 3 || value.length > 20) {
+      return (
+        <div className="alert alert-danger">
+          <span>The username must be between 3 and 20 characters.</span>
+          <a class="close">&times;</a>
+        </div>
+      );
+    }
+  };
+
+
+  const validateEmail = (value) => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+     if (!re.test(String(value).toLowerCase())) {
+       return (
+        <div className="alert alert-danger" >
+          This is not a valid email.
+        </div>
+      );
+     };     
+  };
+
+  const validatePassword = (value) => {
+    if (value.length < 6 || value.length > 40) {
+      return (
+        <div className="alert alert-danger" >
+          The password must be between 6 and 40 characters.
+        </div>
+      );
+    };   
+     
+  }
+
+  
+
+  const confirmPassword = (value,props, components) => {    
+    if (value !== components['password'][0].value) {
+      return (
+        <div className="alert alert-danger" >
+          Passwords are not equal.
+        </div>
+      )
+    }   
+
+  }
+
+  const handleSubmit = (e) => {
+    const {username, email, password, confirm_password} = user;
+    e.preventDefault();
+    form.current.validateAll();
+    if (checkBtn.current.context._errors.length === 0) {
+      axios.post('http://localhost:8080/api/registation', {
+        username, email,  password
+      })
+      .then(function (response) {        
+        setUser({
+          ...user,
+          error: "Sign Up is successed",
+          success: true
+        });
+        console.log(response);
+      })
+      .catch(function (error) {
+        setUser({
+          ...user,
+          error: error.response.data,
+          success: false
+        });
+        console.log(user.error);
+      });
+
+    };   
+
+  }
+
   return (
-    <SignUpForm />
+    <SignUpForm user={user} 
+                refs={{form, checkBtn}}
+                validate={{required, vusername, validateEmail, validatePassword, confirmPassword}}
+                handleChange={handleChange} handleSubmit={handleSubmit}/>
   )
 }
 
