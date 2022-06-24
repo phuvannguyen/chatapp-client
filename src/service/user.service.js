746a5628e99319607ada3ee1a5a9636fbad3c897
unknown <phu21994@gmail.com>
2022-06-24 17:31:58 +0700

    Create getRooms functions

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 const API_URL = "http://localhost:8080/";
+
 const getPublicContent = () => {
   return axios.get(API_URL + "all", { headers: authHeader() });
 };
 
+const getRooms = (id) => {
+  return axios.get(API_URL + "api/" + "rooms",{headers:  authHeader(), params:{_id: id}})
+}
+
+
 const UserService = {
-  getPublicContent
+  getPublicContent,
+  getRooms
   
 };
 export default UserService;
