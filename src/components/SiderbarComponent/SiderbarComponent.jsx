42c4c4ef6e9263a0ac761a35968654eb08d60d49
unknown <phu21994@gmail.com>
2022-06-24 17:25:53 +0700

    Create logic for siderbar component

--- src/components/SiderbarComponent/SiderbarComponent.jsx
+++ src/components/SiderbarComponent/SiderbarComponent.jsx
@@ -3,29 +3,30 @@ import HeaderSideComponnent from '@components/HeaderSideComponet'
 import ContactComponent from '@components/ContactComponent'
 import "./SiderbarComponent.css"
 
-function SiderbarComponent() {
+function SiderbarComponent({contact}) {
+  const { contacts, error, success} = contact;
+
   return (
     <div className="siderbar">
-      <HeaderSideComponnent />     
-      <div className="main_siderbar">
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
-        <ContactComponent />
+      <HeaderSideComponnent />
+      {success && 
+        <div className="main_siderbar">        
+          {contacts.map((value, index) => {
+            return (<ContactComponent value={value} />)         
+            
+
+          })}
       </div>
+      }
+      {/* check error occur when can't reload data from server */}
+      {error && 
+        <div className="main_siderbar">
+          <div className="alert alert-danger">
+            {error}
+          </div>          
+        </div>
+      }    
+
     </div>   
     
   )
--- src/pages/Siderbar/Siderbar.jsx
+++ src/pages/Siderbar/Siderbar.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import SiderbarComponent from '@components/SiderbarComponent'
 import Contact from '@pages/Contact'
+import { useUserContext } from '@contexts/UserContext'
+import UserService from "../../service/user.service"
 
 function Siderbar() {
+  const { userLocal, setUserLocal } = useUserContext();
+  const [contact, setContact] = useState({
+    contacts: [],
+    error: "",
+    success: false 
+  });
+  const { _id } = userLocal;
+  useEffect(() => {
+    UserService.getRooms(_id).
+    then((res) => {
+      setContact({
+        ...contact,
+        contacts: res.data,
+        error:"",
+        success: true
+      });
+    }) 
+    .catch((err) => {
+      setContact({
+        ...contact,
+        error: err.res.data
+      })
+
+    })
+
+  }, []);
+  
+  
   return (
-    <SiderbarComponent />
+    <SiderbarComponent contact={contact}/>
   )
 }
 
