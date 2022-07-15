16176994190b283801161fa5388bd1845588bb6a
unknown <phu21994@gmail.com>
2022-07-15 14:42:42 +0700

    Combine Room context below UserContext

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import UserContextProvider from '@contexts/UserContext'
+import RoomContextProvider from '@contexts/RoomContext'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <UserContextProvider>
-        <App />
-    </UserContextProvider>
-        
+        <RoomContextProvider>
+            <App />
+        </RoomContextProvider>        
+    </UserContextProvider> 
     
   
     
