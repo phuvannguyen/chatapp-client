0b929e20772e35ab2fae8916cef532af36fd0356
unknown <phu21994@gmail.com>
2022-07-15 14:46:24 +0700

    Get id room to room state (Global state)

--- src/components/ContactComponent/ContactComponent.jsx
+++ src/components/ContactComponent/ContactComponent.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import "./ContactComponent.css"
-import { Avatar } from '@mui/material';
+import { Avatar} from '@mui/material';
 
-function ContactComponent({ value }) {
+function ContactComponent({value, handleUserClick}) {
+  const url = value.profile?.avatar_url || ""; //handle case underfind.
   return (    
-    <div className="chat_header">
+    <div  className="chat_header"  onClick={() => handleUserClick(value._id)} role="button" tabIndex={0}>
       <div className='chat_avatar'>
-        <Avatar alt={value.title} src="" />
-        <div className="chat_headerinfo">
-          <h3>{value.title}</h3>
-          <p>Have nothing message</p>                 
-        </div>               
+        <Avatar alt={value.title} src={url} />
+          <div className="chat_headerinfo">
+            <h3>{value.title}</h3>
+            <p>Have nothing message</p>                 
+          </div>                      
 
       </div>
         
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,11 +1,26 @@
 import ContactComponent from "@components/ContactComponent";
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useRoomContext } from "@contexts/RoomContext";
 
-function Contact() {
+function Contact({value}) {
+  const [id, setId] = useState("");
+  const {room, setRoom} = useRoomContext();   
 
   
+  const handleUserClick = (_id) =>  {    
+    setRoom(
+      {
+        roomIdActivated: _id
+      }
+      
+    );  
+      
+    
+
+  }
+  
   return (
-    <ContactComponent />
+    <ContactComponent value={value}  handleUserClick={handleUserClick}/>
   )
 }
 
