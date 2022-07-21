f0822eb5ac8e28fceeb1217f8ec18ecd81aa20a0
unknown <phu21994@gmail.com>
2022-07-21 15:15:14 +0700

    Fix logic

--- src/pages/ChatMain/ChatMain.jsx
+++ src/pages/ChatMain/ChatMain.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import ChatMainComponent from '@components/ChatMainComponent'
+import { useRoomContext } from '@contexts/RoomContext'
+import UserService from "../../service/user.service"
 
 function ChatMain() {
+  const { room } = useRoomContext();
+  const [roomState, setRoomState] = useState({
+    room: {},
+    error: "",
+    success: false 
+  });
+
+  useEffect(() => {
+    UserService.getOneRoom(room.roomIdActivated).
+    then((res) => {
+      setRoomState({
+        ...roomState, 
+        room: res.data,
+        error: "",
+        success: true
+      })
+
+    })
+    .catch((error) => {
+      setRoomState({
+        ...roomState, 
+        error: error.res.data        
+      })
+
+
+    })
+  }, [])
   return (
-    <ChatMainComponent />
+    <ChatMainComponent roomState={roomState}/>
   )
 }
 
