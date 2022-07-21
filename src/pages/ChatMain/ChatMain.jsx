6e6f5d941b65357a7cc814ecf98338f77a0e014a
unknown <phu21994@gmail.com>
2022-07-21 16:04:44 +0700

    Fix mistake wrong component

--- src/pages/ChatMain/ChatMain.jsx
+++ src/pages/ChatMain/ChatMain.jsx
@@ -4,35 +4,8 @@ import { useRoomContext } from '@contexts/RoomContext'
 import UserService from "../../service/user.service"
 
 function ChatMain() {
-  const { room } = useRoomContext();
-  const [roomState, setRoomState] = useState({
-    room: {},
-    error: "",
-    success: false 
-  });
-
-  useEffect(() => {
-    UserService.getOneRoom(room.roomIdActivated).
-    then((res) => {
-      setRoomState({
-        ...roomState, 
-        room: res.data,
-        error: "",
-        success: true
-      })
-
-    })
-    .catch((error) => {
-      setRoomState({
-        ...roomState, 
-        error: error.res.data        
-      })
-
-
-    })
-  }, [])
   return (
-    <ChatMainComponent roomState={roomState}/>
+    <ChatMainComponent />
   )
 }
 
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import MainComponent from '@components/MainComponent'
-
+import { useRoomContext } from '@contexts/RoomContext'
+import UserService from "../../service/user.service"
 
 function Main() {
+  const { room } = useRoomContext();
+  
+  const [roomState, setRoomState] = useState({
+    room: {},
+    error: "",
+    success: false 
+  });
+
+  useEffect(() => {
+    UserService.getOneRoom(room.roomIdActivated).
+    then((res) => {      
+      setRoomState({
+        ...roomState, 
+        room: res.data,
+        error: "",
+        success: true
+      });
+      
+      
+
+    })
+    .catch((error) => {
+      setRoomState({
+        ...roomState, 
+        error: error.res.data        
+      });
+
+
+    })
+  }, [room.roomIdActivated])
   return (
-    <MainComponent />
+    <MainComponent roomState={roomState}/>
   )
 }
 
