95fe5b2c8fd70804613752914c156806054e2e30
unknown <phu21994@gmail.com>
2022-07-25 22:43:58 +0700

    Create logic in this component

--- src/pages/ChatMain/ChatMain.jsx
+++ src/pages/ChatMain/ChatMain.jsx
@@ -4,8 +4,41 @@ import { useRoomContext } from '@contexts/RoomContext'
 import UserService from "../../service/user.service"
 
 function ChatMain() {
+  const [chats, setChats] = useState({
+    content: [],
+    error: "",
+    success: false
+  }) 
+  const {room} = useRoomContext();
+  const roomIdActivated = room.roomIdActivated;  
+
+  useEffect(() => {    
+    UserService.getChats(roomIdActivated).
+    then((res) => {      
+      setChats(
+        {
+          ...chats,
+          content: res.data,
+          success: true
+  
+        }
+      );
+      
+       
+
+    })
+    .catch((error) => {
+      setChats({
+        ...chats,        
+        error: error.res.data
+      });
+     
+    })
+
+
+  }, [roomIdActivated])
   return (
-    <ChatMainComponent />
+    <ChatMainComponent chats={chats}/>
   )
 }
 
