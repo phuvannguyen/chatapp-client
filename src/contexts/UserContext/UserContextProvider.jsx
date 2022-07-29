a945ecc0d3d19bc3624c1ffb3288864314129c28
unknown <phu21994@gmail.com>
2022-07-29 13:03:39 +0700

    Fix error

--- src/contexts/UserContext/UserContextProvider.jsx
+++ src/contexts/UserContext/UserContextProvider.jsx
@@ -4,7 +4,8 @@ import UserContext from "./UserContext";
 function UserContextProvider({ children }){
   const data = JSON.parse(localStorage.getItem("user"));
   let userGlobal;
-  const { user, token } = data;  
+  const user = data?.user || undefined;
+  const token = data?.token || undefined;  
   user && token ? userGlobal =  user : userGlobal = "";   
 
   const [userLocal, setUserLocal] = useState({
