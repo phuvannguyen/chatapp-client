d65b5efe366321c9dd6bae5d58455e951e42b2c0
unknown <phu21994@gmail.com>
2022-08-21 19:50:05 +0700

    Add messages to room component

--- src/components/RoomComponent/RoomComponent.jsx
+++ src/components/RoomComponent/RoomComponent.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import "./RoomComponent.css"
 import { Avatar} from '@mui/material';
 
-function RoomComponent({value, handleUserClick}) {  
-  const url = value?.profile?.avatar_url || ""; //handle case underfind.
+function RoomComponent({value, handleUserClick, chat}) {  
+  const url = value?.profile?.avatar_url || "";
+  const content = chat[chat.length - 1]?.content || "Have nothing messages" //handle case underfind.
+  
   return (    
     <div  className="chat_header"  onClick={(e) => handleUserClick(value._id, e)} role="button" tabIndex={0}>
       <div className='chat_avatar'>
         <Avatar alt={value.title} src={url} />
           <div className="chat_headerinfo">
             <h3>{value.title}</h3>
-            <p>Have nothing message</p>                 
+            <p>{content}</p>                 
           </div>                      
 
       </div>
--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -2,23 +2,40 @@ import RoomComponent from "@components/RoomComponent";
 import React, { useEffect, useState } from 'react'
 import { useRoomContext } from "@contexts/RoomContext";
 import { useNavigate } from 'react-router-dom';
+import { useParams } from "react-router-dom";
+import UserService from '../../service/user.service';
+
 
 function Room({value}) {
   const { room, setRoom } = useRoomContext();
-  
-  let navigate = useNavigate(); 
+  const [chat, setChat] = useState("");  
+  let navigate = useNavigate();
+ 
   
   const handleUserClick = (_id, e) =>  {    
     e.preventDefault();        
     setRoom({roomIdActivated: _id});    
-    navigate(`/chat/room/${_id}`);     
-    
-
+    navigate(`/chat/room/${_id}`);
   }
+  
+  useEffect(() => {
+   
+    UserService.getChats(value._id)
+    .then((res) => {
+      console.log(res.data);
+      setChat(res.data)
+
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }, []);    
+
+  
   
   
   return (
-    <RoomComponent value={value}  handleUserClick={handleUserClick}/>
+    <RoomComponent value={value}  handleUserClick={handleUserClick} chat={chat}/>
     
   )
 }
