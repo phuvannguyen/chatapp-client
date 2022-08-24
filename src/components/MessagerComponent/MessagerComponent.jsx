3632e7eed09fabefa1ca62a65cf01d0f3120ceb2
unknown <phu21994@gmail.com>
2022-08-24 18:12:54 +0700

    Fix UI of receiveder and sender

--- src/components/MessagerComponent/MessagerComponent.jsx
+++ src/components/MessagerComponent/MessagerComponent.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useParams } from 'react-router-dom';
+import { useUserContext } from '@contexts/UserContext';
 import "./MessagerComponent.css"
 
 function MessagerComponent({chat, error, success}) {
-    let { id } = useParams(); 
+    const {userLocal} = useUserContext();
+    console.log(userLocal) 
     
     const username = chat.profile?.name || chat.owner.username;
-    const idChater = chat.id;
+    const idChater = chat.owner._id;
     const content = chat.content;
     const timeChat = new Date(chat.createdAt).toUTCString();
        
@@ -22,7 +24,7 @@ function MessagerComponent({chat, error, success}) {
         <span className="timestamp">{currentTime}</span>
       </p>}
       {success && 
-      <p className={id === idChater ? "chat_message": "chat_message chat_receiver"}>
+      <p className={userLocal._id === idChater ? "chat_message": "chat_message chat_receiver"}>
         <span className="username">{username}</span>
             {content}
         <span className="timestamp">{timeChat}</span>
