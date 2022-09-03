2f1a5ad987b0231dd771470c468e59653126f359
unknown <phu21994@gmail.com>
2022-09-03 14:43:13 +0700

    Change headers

--- src/components/HeaderMainComponent/HeaderMainComponent.jsx
+++ src/components/HeaderMainComponent/HeaderMainComponent.jsx
@@ -5,14 +5,15 @@ import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined'
 import AttachFileIcon from '@mui/icons-material/AttachFile'
 import PeopleOutlineIcon from '@mui/icons-material/PeopleOutline';
 
-function HeaderMainComponent({title}) {
+function HeaderMainComponent({titleHeader}) {  
+  
   const currentTime = new Date().toString() 
   return (
     <div className="header-main">
       <div className="usermain-info">
         <Avatar alt="ngaoasen" src="" />
         <div className="username">
-          <h3>{title}</h3>
+          <h3>{titleHeader}</h3>
           <p>{currentTime}</p>
         </div>
       </div>
--- src/pages/HearderMain/HeaderMain.jsx
+++ src/pages/HearderMain/HeaderMain.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import HeaderMainComponent from '@components/HeaderMainComponent'
+import { useUserContext } from '@contexts/UserContext';
+import UserService from '../../service/user.service';
 function HeaderMain({roomState}) {
+  const {userLocal} = useUserContext();
+  const idUser = userLocal._id;
+  const {room} = roomState;
   
-  const title = roomState.room?.title || "No Names";
+  const {title} = room;  
+  const [titleHeader, setTitleHeader] = useState("No Name");
+  
+  
+  useEffect(() => {
+    
+    if (room.title) {
+      let usersList = [...room.member, room.owner];
+      let filteredUsers = usersList.filter(id => id !== idUser);
+      let data = [];
+      let setTitle;
+      for (let userId of filteredUsers) {
+        UserService.getUser(userId)
+          .then(user => {
+            data.push(user.data);
+          })
+      };      
+      setTitle = data.length === 1 ?  data[0].profile?.name || data[0].username : title;
+      setTitleHeader(setTitle);   
+  }}, [room]); 
   
   return (
-    <HeaderMainComponent title={title}/>
+    <HeaderMainComponent titleHeader={titleHeader}/>
   )
 }
 
